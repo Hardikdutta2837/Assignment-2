fc1a93cfe355a503e823a8fa8b7fbaa869a7ff04
Hardikdutta2837 <118710608+Hardikdutta2837@users.noreply.github.com>
2022-12-21 22:11:19 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,9 @@
+a="Python is a case sensitive language"
+print(len(a))
+print(a[::-1])
+b=a[10:26]
+print(b)
+c="object oriented"
+d=a.replace(b,c)
+print(d)
+print(a.find("a"))
---
+++ Q2.py
@@ -0,0 +1,5 @@
+name="Hardik Dutta"
+sid=22105095
+dept="Electronics and Communication"
+cg=9.5
+print(" Hey,",name,"Here!\n","My SID is",sid,"\n","I am from",dept,"department and my CGPA is",cg)
